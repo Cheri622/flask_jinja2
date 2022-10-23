b71bbc2614f09dcd416f55ec7c3e066176588898
Cheri622 <95024780+Cheri622@users.noreply.github.com>
2022-10-23 12:05:08 +0800

    Update sqlapp.py

--- flask/sqlapp.py
+++ flask/sqlapp.py
@@ -1,9 +1,8 @@
+#author: Cheri Chen
 import os
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
-
-#basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 Bootstrap(app)
