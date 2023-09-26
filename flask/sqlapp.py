7195a8a9568f1bcaf4ab4bb5095a801fe0b33ca9
Cheri622 <95024780+Cheri622@users.noreply.github.com>
2023-09-26 09:17:16 +0800

    Update sqlapp.py

--- flask/sqlapp.py
+++ flask/sqlapp.py
@@ -9,14 +9,13 @@ Bootstrap(app)
 
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://xxx:xxx@localhost:3306/"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://xxx:xxx@xxx:3306/"
 
 # this variable, db, will be used for all SQLAlchemy commands
 db = SQLAlchemy(app)
 
 
 # each table in the database needs a class to be created for it
-# db.Model is required - don't change it
 # identify all columns by name and data type
 class City(db.Model):
     __tablename__ = 'city'
@@ -24,22 +23,14 @@ class City(db.Model):
     name = db.Column(db.String)
     countrycode = db.Column(db.String, db.ForeignKey('country.code'))
     district = db.Column(db.String)
-    population = db.Column(db.Integer)
-    
+   
 
 class Country(db.Model):
     __tablename__ = 'country'
     code = db.Column(db.String, primary_key=True)
     name = db.Column(db.String)
     continent = db.Column(db.String)
-    region = db.Column(db.String)
-    surfacearea = db.Column(db.String)
-    indepyear = db.Column(db.Integer)
-    population = db.Column(db.Integer)
-    lifeexpectancy = db.Column(db.Float)
     gnp = db.Column(db.Float)
-
-    
     
     
 @app.route('/home',methods=['POST','GET'])
@@ -48,7 +39,6 @@ def home():
         if request.values['send'] == 'send':
             cname = request.form["city"]
             icity = City.query.filter(City.name==cname)
-            icountry = Country.query.filter(Country.code==icity.countrycode)
             return render_template('home-1.html', city = icity)
     return render_template('home.html')    
  
